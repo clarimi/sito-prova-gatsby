69ad8f020e69a94ab00bab90737e010b6e2a2ec8
clarimi <mirandolaclarice@gmail.com>
2022-06-21 22:15:00 +0200

    Corretto errore

--- src/scripts/preloader.js
+++ src/scripts/preloader.js
@@ -1,41 +1,52 @@
 import { gsap, Power1 } from "gsap";
 
 export function animaPreloader(animaCopertine) {
-    var body = document.querySelector("body");
+    if (typeof document !== `undefined`) {
 
-    gsap.to("#preloader", {
-        opacity: 0,
-        duration: 0.8,
-        delay: 2,
-        onStart: () => {
-            if (animaCopertine) {
-                animaCopertine();
-            }
-        },
-        onComplete: () => {
-            body.classList.remove("preloader_active");
-            body.classList.remove("preloader_ready");
-        },
-        ease: Power1.easeOut
-    });
+        // Do something with the document
+        // Call your modules and libraries
+        var body = document.querySelector("body");
 
-    gsap.to("#the", {
-        y: -400,
-        duration: 1,
-        delay: 1,
-        ease: Power1.easeOut
-    });
+        gsap.to("#preloader", {
+            opacity: 0,
+            duration: 0.8,
+            delay: 2,
+            onStart: () => {
+                if (animaCopertine) {
+                    animaCopertine();
+                }
+            },
+            onComplete: () => {
+                body.classList.remove("preloader_active");
+                body.classList.remove("preloader_ready");
+            },
+            ease: Power1.easeOut
+        });
 
-    gsap.to("#veroneser", {
-        y: 400,
-        duration: 1,
-        delay: 1,
-        ease: Power1.easeOut
-    })
-    gsap.to("#testocentro", {
-        opacity: 0,
-        duration: 1,
-        delay: 1,
-        ease: Power1.easeOut
-    })
+        gsap.to("#the", {
+            y: -400,
+            duration: 1,
+            delay: 1,
+            ease: Power1.easeOut
+        });
+
+        gsap.to("#veroneser", {
+            y: 400,
+            duration: 1,
+            delay: 1,
+            ease: Power1.easeOut
+        })
+        gsap.to("#testocentro", {
+            opacity: 0,
+            duration: 1,
+            delay: 1,
+            ease: Power1.easeOut
+        })
+
+    } else {
+        return {
+            // Do something different while window and document are not defined on the server
+        };
+    }
+
 }
