e00823c505a8ac058fe9d36a252ab6b5af47f5e1
Mattia Corciulo <mattia.corciulo@gmail.com>
2022-08-27 16:49:14 +0200

    aggiunto google tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
     siteMetadata: {
-        title: `Sito Prova`,
-        siteUrl: `https://www.yourdomain.tld`
+        title: `The Veroneser`,
+        siteUrl: `https://www.theveroneser.it`
     },
     plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-react-helmet", "gatsby-plugin-transition-link", {
         resolve: 'gatsby-source-filesystem',
@@ -18,5 +18,31 @@ module.exports = {
                 accessToken: "wAGXwStYwQxFsRxoxmuITuyXdx12bRTvOzJr_WAPd9g",
             },
             __key: "images"
-        }]
+        },
+        {
+            resolve: "gatsby-plugin-google-tagmanager",
+            options: {
+              id: "GTM-57ZNKG6",
+        
+              // Include GTM in development.
+              //
+              // Defaults to false meaning GTM will only be loaded in production.
+              includeInDevelopment: false,
+        
+              // datalayer to be set before GTM is loaded
+              // should be an object or a function that is executed in the browser
+              //
+              // Defaults to null
+              defaultDataLayer: { platform: "gatsby" },
+        
+            //   // Specify optional GTM environment details.
+            //   gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+            //   gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+            //   dataLayerName: "YOUR_DATA_LAYER_NAME",
+        
+              // Defaults to https://www.googletagmanager.com
+              selfHostedOrigin: "https://www.theveroneser.it",
+            },
+          },
+    ]
 };
--- src/components/NavbarHeaderContainer.js
+++ src/components/NavbarHeaderContainer.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import "../styles/style.scss"
-import { Helmet } from "react-helmet"
+import { Helmet } from "react-helmet";
+import { ethers } from "ethers";
 import NavbarHeader from "../components/NavbarHeader"
 import NavbarHeaderMobile from "../components/NavbarHeaderMobile"
 
@@ -8,9 +9,43 @@ import NavbarHeaderMobile from "../components/NavbarHeaderMobile"
 
 
 class NavbarHeaderContainer extends React.Component {
+    isLoggedIn = false;
+
+    accediAMetamask() {
+        console.log("faccio cose");
+        // A Web3Provider wraps a standard Web3 provider, which is
+        // what MetaMask injects as window.ethereum into each page
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+        // MetaMask requires requesting permission to connect users accounts
+        provider.send("eth_requestAccounts", []).then((accounts) => {
+            console.log("Autenticato su MetaMask");
+            console.log(accounts);
+            // The MetaMask plugin also allows signing transactions to
+            // send ether and pay to change state within the blockchain.
+            // For this, you need the account signer...
+            const signer = provider.getSigner();
+            console.log("signer", signer);
+            this.isLoggedIn = true;
+            this.render();
+        })
+
+
+    }
     render() {
+
+        if (typeof window.ethereum !== 'undefined') {
+            console.log('MetaMask is installed!');
+        }
+
         return (
             <main>
+                {!this.isLoggedIn && (
+                    <button onClick={() => this.accediAMetamask()} className="metamask-login-btn">Accedi con MetaMask</button>
+                )}
+                {this.isLoggedIn && (
+                    <a className="metamask-avatar">User avatar</a>
+                )}
                 <div className="no-mobile">
                     <NavbarHeader></NavbarHeader>
                 </div>
